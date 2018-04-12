320ebe1e1a4ea6b35562e4b14314c7c1af216a47
Vamsi Devalla <vammichanti40312@gmail.com>
2018-04-12 10:34:43 -0400

    continuing the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 
-
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { NewComponentComponent } from './components/new-component/new-component.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    NewComponentComponent
   ],
   imports: [
     BrowserModule,
